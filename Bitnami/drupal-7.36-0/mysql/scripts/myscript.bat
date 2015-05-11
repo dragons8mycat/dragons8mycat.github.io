40a2f051ef19ef729627861e43611e5aafdd0d27
dragons8mycat <dragons8mycat@live.co.uk>
2015-05-11 14:38:24 +0100

    take out drupal
    yup, didnt work!

--- Bitnami/drupal-7.36-0/mysql/scripts/myscript.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:\Bitnami\drupal-7.36-0/mysql\bin\mysql.exe" --defaults-file="C:\Bitnami\drupal-7.36-0/mysql\my.ini" -u root -e "UPDATE mysql.user SET Password=password('%1') WHERE User='root';"
-"C:\Bitnami\drupal-7.36-0/mysql\bin\mysql.exe" --defaults-file="C:\Bitnami\drupal-7.36-0/mysql\my.ini" -u root -e "UPDATE mysql.user SET Password=password('%1') WHERE User='';"
-"C:\Bitnami\drupal-7.36-0/mysql\bin\mysql.exe" --defaults-file="C:\Bitnami\drupal-7.36-0/mysql\my.ini" -u root -e "FLUSH PRIVILEGES;"
--- Bitnami/drupal-7.36-0/use_drupal.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-CALL "C:\Bitnami\DRUPAL~1.36-\scripts\setenv.bat"
-C:
-cd "C:\Bitnami\drupal-7.36-0"
-START "Bitnami Drupal Stack Environment" cmd
-                    
